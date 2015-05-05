079336ab4c481de58825a5733d4c7334a93ea869
root <root@raspberrypi.(none)>
2015-05-04 23:14:44 -0500

    Python script functioning as intended! Adding accessory functions.

--- philcar.py
+++ philcar.py
@@ -4,25 +4,20 @@ import time
 from time import strftime, localtime
 import datetime
 import serial
+import picamera
+
 try:
     import RPi.GPIO as GPIO
 except RuntimeError:
     print "Error importing RPi.GPIO! This is probably because you need superuser privileges. You can achieve this by using 'sudo' to run your script."
-#import xively
-
-"""
-XIVELY_API_KEY = 'GR5ACBtf00P6nTxy0svr5Eqb3eWQgXOP7Pv7FtOQPWR7ZPG1'
-XIVELY_FEED_ID = '69373229'
-api = xively.XivelyAPIClient(XIVELY_API_KEY)
-feed = api.feeds.get(XIVELY_FEED_ID)
-"""
+import xively
 
 debugMode = True
 
-dateTimeNow = strftime('%c', localtime())
+dateTimeLong = strftime('%c', localtime())
 
 if debugMode == True:
-    print dateTimeNow
+    print dateTimeLong
     print
 
 GPIO.setmode(GPIO.BOARD)
@@ -44,7 +39,6 @@ def mainLoop():
                 syncDelay()
             for x in range(1, 4):
                 sensorData = getSensorData(x)
-                #print "Sensor data: " + str(sensorData) + " (" + str(x) + ")"
                 
                 time.sleep(3)
             print "Data acquisition complete. Sleeping for 5 seconds."
@@ -57,7 +51,7 @@ def mainLoop():
     
 def getSensorData(cmd):
     data = []
-
+    
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
 
     if cmd == 1:
@@ -86,8 +80,15 @@ def getSensorData(cmd):
         timeString = ser.readline().strip('\r\n')
         data.append(timeString)
 
-    parseString(data, cmd)
-    #return data
+    elif cmd == 4:
+        ser.write('4')
+        st()
+        captureImage()
+        if debugMode == True:
+            print "IR LED check performed. Capturing image."
+        
+    if cmd != 4:
+        parseString(data, cmd)
 
 def parseString(data, cmd):
     global dataUpdated
@@ -105,17 +106,28 @@ def parseString(data, cmd):
         gpsLat = gpsLat.strip("['")
         gpsLon = gpsLon.strip("'")
         gpsCourse = gpsCourse.strip("']")
-
-        print "GPS"
-        print "Data Updated:  " + str(dataUpdated)
-        print "GPS Latitude:  " + str(gpsLat)
-        print "GPS Longitude: " + str(gpsLon)
-        print "Satellites:    " + str(satellites)
-        print "HDOP:          " + str(hdop)
-        print "Altitude (ft): " + str(gpsAltitudeFt)
-        print "Speed (MPH):   " + str(gpsSpeedMPH)
-        print "Course:        " + str(gpsCourse)
-        print
+        if debugMode == True:
+            print "GPS"
+            print "Data Updated:  " + str(dataUpdated)
+            print "GPS Latitude:  " + str(gpsLat)
+            print "GPS Longitude: " + str(gpsLon)
+            print "Satellites:    " + str(satellites)
+            print "HDOP:          " + str(hdop)
+            print "Altitude (ft): " + str(gpsAltitudeFt)
+            print "Speed (MPH):   " + str(gpsSpeedMPH)
+            print "Course:        " + str(gpsCourse)
+            print
+        xivelyData = [int(dataUpdated), float(gpsLat), float(gpsLon), int(satellites), int(hdop),
+                      float(gpsAltitudeFt), float(gpsSpeedMPH), float(gpsCourse)]
+        try:
+            xivelyUpdate(xivelyData)
+            if debugMode == True:
+                print "Data successfully uploaded to Xively."
+                print
+        except:
+            if debugMode == True:
+                print "Data failed to upload to Xively."
+                print
         
     elif cmd == 2:
         accelX, accelY, accelZ = str(data[0]).split(",")
@@ -123,23 +135,24 @@ def parseString(data, cmd):
         gyroX, gyroY, gyroZ = str(data[2]).split(",")
         accelX = accelX.strip("['")
         accelZ = accelZ.strip("'")
-        print "ACCELEROMETER"
-        print "X: " + str(accelX)
-        print "Y: " + str(accelY)
-        print "Z: " + str(accelZ)
-        print "COMPASS"
-        compX = compX.strip("'")
-        compZ = compZ.strip("'")
-        print "X: " + str(compX)
-        print "Y: " + str(compY)
-        print "Z: " + str(compZ)
-        print "GYROSCOPE"
-        gyroX = gyroX.strip("'")
-        gyroZ = gyroZ.strip("']")
-        print "X: " + str(gyroX)
-        print "Y: " + str(gyroY)
-        print "Z: " + str(gyroZ)
-        print
+        if debugMode == True:
+            print "ACCELEROMETER"
+            print "X: " + str(accelX)
+            print "Y: " + str(accelY)
+            print "Z: " + str(accelZ)
+            print "COMPASS"
+            compX = compX.strip("'")
+            compZ = compZ.strip("'")
+            print "X: " + str(compX)
+            print "Y: " + str(compY)
+            print "Z: " + str(compZ)
+            print "GYROSCOPE"
+            gyroX = gyroX.strip("'")
+            gyroZ = gyroZ.strip("']")
+            print "X: " + str(gyroX)
+            print "Y: " + str(gyroY)
+            print "Z: " + str(gyroZ)
+            print
         
     elif cmd == 3:
         contDate, contTime = str(data).split(",")
@@ -149,18 +162,56 @@ def parseString(data, cmd):
         contYear = contYear.strip("'")
         contHour = contHour.strip(" '")
         contSecond = contSecond.strip("']")
-        print "CONTROL UNIT DATE/TIME"
-        print "Month:  " + str(contMonth)
-        print "Day:    " + str(contDay)
-        print "Year:   " + str(contYear)
-        print "Hour:   " + str(contHour)
-        print "Minute: " + str(contMinute)
-        print "Second: " + str(contSecond)
-        print
+        if debugMode == True:
+            print "CONTROL UNIT DATE/TIME"
+            print "Month:  " + str(contMonth)
+            print "Day:    " + str(contDay)
+            print "Year:   " + str(contYear)
+            print "Hour:   " + str(contHour)
+            print "Minute: " + str(contMinute)
+            print "Second: " + str(contSecond)
+            print
         
     else:
-        print "Invalid command."
-        print
+        if debugMode == True:
+            print "Invalid command."
+            print
+
+def xivelyUpdate(xivelyData):
+    api = xively.XivelyAPIClient("MKPFAnS47P9FJAV2D7vw5M9MmHWdsEnj7zuCuJiaoyvua8jO")
+    feed = api.feeds.get(1352564954)
+    streamList = [dataUpdated, gpsLat, gpsLon, satellites, hdop,
+                  gpsAltitudeFt, gpsSpeedMPH, gpsCourse]
+    feed.datastreams = [
+        xively.Datastreams(id=streamList[0], current_value=, at=now),
+        xively.Datastreams(id=streamList[1], current_value=, at=now),
+        xively.Datastreams(id=streamList[2], current_value=, at=now),
+        xively.Datastreams(id=streamList[3], current_value=, at=now),
+        xively.Datastreams(id=streamList[4], current_value=, at=now),
+        xively.Datastreams(id=streamList[5], current_value=, at=now),
+        xively.Datastreams(id=streamList[6], current_value=, at=now),
+        xively.Datastreams(id=streamList[7], current_value=, at=now)
+    ]
+
+def captureImage():
+    localroot = '/home/phil/datsun/'
+    filename = localroot + timeStamp() + '.jpg'
+    with picamera.PiCamera() as camera:
+        camera.resolution = (640, 480)
+        camera.start_preview()
+        time.sleep(4)
+        camera.capture(filename)
+        camera.stop_preview()
+
+def timeStamp():
+    month = strftime('%m', localtime())
+    day = strftime('%d', localtime())
+    year = strftime('%y', localtime())
+    hour = strftime('%H', localtime())
+    minute = strftime('%M', localtime())
+    second = strftime('%S', localtime())
+    currentDateTime = month + day + year + "-" + hour + minute + second
+    return currentDateTime
 
 def syncDelay():
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
