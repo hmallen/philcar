2c843b18dadbddd720b1d399357bca3deff9146f
hmallen <allenhm@gmail.com>
2015-05-06 05:10:15 -0500

    Attempts at maps...

--- site/js/main.js
+++ site/js/main.js
@@ -34,4 +34,12 @@ $(document).ready(function ($) {
         	$(satellites).html(datastream["datastreams"]["7"]["current_value"]);   
         });
     });
+	function initialize() {
+        var mapOptions = {
+        zoom: 8,
+        center: new google.maps.LatLng(gpsLat, gpsLon)
+    	};
+    	map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
 });
