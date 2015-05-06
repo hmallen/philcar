ad32f538d773d333bc839d6751aec27bc45b9add
hmallen <allenhm@gmail.com>
2015-05-06 04:42:23 -0500

    Restructuring Google Maps API calls on load.

--- site/js/main.js
+++ site/js/main.js
@@ -35,6 +35,13 @@ $(document).ready(function ($) {
         $(gpsCourse).html(datastream["datastreams"]["7"]["current_value"]);
         });
     });
-	var mapsLink = "https://www.google.com/maps/embed/v1/place?key=AIzaSyBQCYHsfgcnL6Gj2uckoaeN6PIWonWHDgg&q=" + gpsLat + "," + gpsLon
-	$(mapsLink).html(mapsLink);
+
+	function initialize() {
+		var mapOptions = {
+    	zoom: 8,
+    	center: new google.maps.LatLng(gpsLat, gpsLon)
+  	};
+ 	map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+	}
+	google.maps.event.addDomListener(window, 'load', initialize);
 });
