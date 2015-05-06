062bdf0bf4e70c9b44e57c1ac3f9b7a99241cccb
hmallen <allenhm@gmail.com>
2015-05-06 04:10:05 -0500

    Updated xively js.

--- site/js/xively-feed.js
+++ site/js/xively-feed.js
@@ -4,34 +4,34 @@ $(document).ready(function ($) {
     console.log("Key found. Setting JQuery");
 
     var feedID = 1352564954,
-            dataUpdated = "#dataUpdated",
-            gpsLat = "#gpsLat",
-            gpsLon = "#gpsLon",
-            satellites = "#satellites",
-            hdop = "#hdop",
-            gpsAltitudeFt = "#gpsAltitudeFt",
-            gpsSpeedMPH = "#gpsSpeedMPH",
-            gpsCourse = "#gpsCourse";
+        dataUpdated = "#dataUpdated",
+        gpsLat = "#gpsLat",
+        gpsLon = "#gpsLon",
+        satellites = "#satellites",
+        hdop = "#hdop",
+        gpsAltitudeFt = "#gpsAltitudeFt",
+        gpsSpeedMPH = "#gpsSpeedMPH",
+        gpsCourse = "#gpsCourse";
 
     xively.feed.get(feedID, function (datastream) {
-        $(dataUpdated).html(datastream["datastreams"]["dataUpdated"]["current_value"]);
-        $(gpsLat).html(datastream["datastreams"]["gpsLat"]["current_value"]);
-        $(gpsLon).html(datastream["datastreams"]["gpsLon"]["current_value"]);
-        $(satellites).html(datastream["datastreams"]["satellites"]["current_value"]);
-        $(hdop).html(datastream["datastreams"]["hdop"]["current_value"]);
-        $(gpsAltitudeFt).html(datastream["datastreams"]["gpsAltitudeFt"]["current_value"]);
-        $(gpsSpeedMPH).html(datastream["datastreams"]["gpsSpeedMPH"]["current_value"]);
-        $(gpsCourse).html(datastream["datastreams"]["gpsCourse"]["current_value"]);
+        $(dataUpdated).html(datastream["datastreams"]["0"]["current_value"]);
+        $(gpsLat).html(datastream["datastreams"]["1"]["current_value"]);
+        $(gpsLon).html(datastream["datastreams"]["2"]["current_value"]);
+        $(satellites).html(datastream["datastreams"]["3"]["current_value"]);
+        $(hdop).html(datastream["datastreams"]["4"]["current_value"]);
+        $(gpsAltitudeFt).html(datastream["datastreams"]["5"]["current_value"]);
+        $(gpsSpeedMPH).html(datastream["datastreams"]["6"]["current_value"]);
+        $(gpsCourse).html(datastream["datastreams"]["7"]["current_value"]);
 
         xively.feed.subscribe(feedID, function (event, datastream_updated) {
-            $(dataUpdated).html(datastream["datastreams"]["dataUpdated"]["current_value"]);
-            $(gpsLat).html(datastream["datastreams"]["gpsLat"]["current_value"]);
-            $(gpsLon).html(datastream["datastreams"]["gpsLon"]["current_value"]);
-            $(satellites).html(datastream["datastreams"]["satellites"]["current_value"]);
-            $(hdop).html(datastream["datastreams"]["hdop"]["current_value"]);
-            $(gpsAltitudeFt).html(datastream["datastreams"]["gpsAltitudeFt"]["current_value"]);
-            $(gpsSpeedMPH).html(datastream["datastreams"]["gpsSpeedMPH"]["current_value"]);
-        $(gpsCourse).html(datastream["datastreams"]["gpsCourse"]["current_value"]);
+            $(dataUpdated).html(datastream["datastreams"]["0"]["current_value"]);
+            $(gpsLat).html(datastream["datastreams"]["1"]["current_value"]);
+            $(gpsLon).html(datastream["datastreams"]["2"]["current_value"]);
+            $(satellites).html(datastream["datastreams"]["3"]["current_value"]);
+            $(hdop).html(datastream["datastreams"]["4"]["current_value"]);
+            $(gpsAltitudeFt).html(datastream["datastreams"]["5"]["current_value"]);
+            $(gpsSpeedMPH).html(datastream["datastreams"]["6"]["current_value"]);
+        $(gpsCourse).html(datastream["datastreams"]["7"]["current_value"]);
         });
     });
 });
